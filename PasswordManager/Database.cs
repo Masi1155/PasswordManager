46238b7856d6de49f7afd3a13ef9baa0506178ed
Marcel.Kunert <masi.kun@live.de>
2023-04-25 07:59:08 +0200

    redid DB

--- PasswordManager/Database.cs
+++ PasswordManager/Database.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace PasswordManager
 {
     internal class Database
     {
+        public List<Folder> folders= new List<Folder>();
+        public String path;
+
+        public Database()
+        {
+
+        }
+
+        public void SaveToFile()
+        {
+            using (FileStream fs = new(this.path, FileMode.OpenOrCreate))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Database));
+                xmlSerializer.Serialize(fs,this);
+            }
+        }
+        public Database LoadFromFile()
+        {
+            using (FileStream fs = new(this.path, FileMode.Open))
+            {
+                XmlSerializer xmlSerializer= new XmlSerializer(typeof(Database));
+                return (Database) xmlSerializer.Deserialize(fs);
+            }
+        }
     }
 }
